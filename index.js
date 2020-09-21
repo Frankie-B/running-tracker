144d72b15db1cc2b4302d057a52d2fcf05a73c88
Frankie Bukenya <frankie.bukenya@gmail.com>
2020-09-21 15:49:50 +0200

    Add out values

--- index.js
+++ index.js
@@ -1,27 +1,54 @@
+const goal = 25;
 let entries = [];
 
 
 const entriesWrapper = document.querySelector('#entries');
+document.querySelector('#target').innerText = goal;
 
 const addNewEntry = (newEntry) => {
   entriesWrapper.removeChild(entriesWrapper.firstElementChild);
-  // console.log(newEntry);
+
   const listItem = document.createElement('li');
-  const listValue = document.createTextNode(newEntry);
+  const listValue = document.createTextNode(newEntry.toFixed(1));
 
   listItem.appendChild(listValue);
   entriesWrapper.appendChild(listItem);
 }
 
+const reducer = (total, currentValue) => {
+  return total + currentValue;
+};
+
+const calcTotal = () => {
+  const totalValue = entries.reduce(reducer).toFixed(1);
+  document.querySelector('#total').innerText = totalValue
+  document.querySelector('#progressTotal').innerText = totalValue
+
+};
+
+const calcAverage = () => {
+  const average = (entries.reduce(reducer) / entries.length).toFixed(1);
+  document.querySelector('#average').innerText = average;
+}
+
+const weeklyHigh = () => {
+  const high = Math.max(...entries)
+  document.querySelector('#high').innerText = high;
+}
+
 const handleSubmit = (e) => {
   e.preventDefault();
   const entry = Number(document.querySelector('#entry').value);
-  // console.log(entry);
   if (!entry) return;
+
   document.querySelector('form').reset()
+
   entries.push(entry);
-  // console.log(entries);
   addNewEntry(entry);
+
+  calcTotal();
+  calcAverage();
+  weeklyHigh();
 };
 
 const form = document.querySelector('form').addEventListener('submit', handleSubmit);
